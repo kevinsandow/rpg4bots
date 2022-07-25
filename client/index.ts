31ef2b32e669124a4b981c7906a1d6a2827c83d0
Kevin Sandow <kevin@sandow.email>
2022-07-25 15:07:41 +0200

    Convert ping to keep a sliding window

--- client/index.ts
+++ client/index.ts
@@ -11,12 +11,29 @@ const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(uri, {
   },
 })
 
-const start = Date.now()
+const pings: number[] = []
 
-socket.emit('ping', () => {
-  // eslint-disable-next-line no-console
-  console.log(`Ping: ${Date.now() - start}ms`)
-})
+function updatePing() {
+  const start = Date.now()
+
+  socket.emit('ping', () => {
+    pings.push(Date.now() - start)
+
+    while (pings.length > 10) {
+      pings.shift()
+    }
+
+    const ping = Math.round(pings.reduce((a, b) => a + b) / pings.length)
+    const delta = Math.round(pings.map((d) => Math.abs(d - ping)).reduce((a, b) => a + b) / pings.length)
+
+    // eslint-disable-next-line no-console
+    console.log(`Ping: ${ping}ms ±${delta}ms (${JSON.stringify(pings)})`)
+  })
+
+  setTimeout(updatePing, 10000)
+}
+
+setTimeout(updatePing, 1000)
 
 socket.emit('characterList', (characters) => {
   // eslint-disable-next-line no-console
